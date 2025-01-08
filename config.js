6a19705d108d793a5b6bb9531bf83762a7db74e5
hebotkr <heprabathmdacc@gmail.com>
2025-01-08 22:15:57 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "6BdEyIrI#gkn6rr2dAb9WnhK3c2DEoZGfx2tRgcICtZJKmUO2mvM"
-ALIVE_IMG:process.env.ALIVE_IMG || "",
-ALIVE_MSG:process.evn.ALIVE_MSG || "HI",    
+SESSION_ID: process.env.SESSION_ID || "6BdEyIrI#gkn6rr2dAb9WnhK3c2DEoZGfx2tRgcICtZJKmUO2mvM",
+ALIVE_IMG:process.env.ALIVE_IMG || "jkdkdk.jpg",
+ALIVE_MSG:process.evn.ALIVE_MSG || "HI‼️",    
 };
